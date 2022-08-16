79b96a058852d4fe916a042576c58e7313d8424f
凤之兮原 <1577310596@qq.com>
2022-08-16 21:50:29 +0800

    refactor: remove local variable

--- src/statement.ts
+++ src/statement.ts
@@ -37,15 +37,13 @@ export function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
 
-    let thisAmount = amountFor(perf);
-
     volumeCredits += Math.max(perf.audience - 30, 0)
 
     if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
 
-    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`
 
-    totalAmount += thisAmount
+    totalAmount += amountFor(perf)
   }
 
   result += `Amount owed is ${format(totalAmount / 100)}\n`
