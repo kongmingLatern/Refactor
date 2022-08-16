a7323d47290137cb46800c224cb5f52b04fe7b6a
凤之兮原 <1577310596@qq.com>
2022-08-16 22:06:57 +0800

    refactor: remove local variable

--- src/statement.ts
+++ src/statement.ts
@@ -64,12 +64,8 @@ export function statement(invoice, plays) {
     totalAmount += amountFor(perf)
   }
 
-  let volumeCredits = totalVolumeCredits();
-
   result += `Amount owed is ${usd(totalAmount / 100)}\n`
-  result += `You earned ${volumeCredits} credits\n`
+  result += `You earned ${totalVolumeCredits()} credits\n`
 
   return result
-
-
 }
