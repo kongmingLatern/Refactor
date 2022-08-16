e1db49dfc4c698d70e206e8312e9f663034dcccb
凤之兮原 <1577310596@qq.com>
2022-08-16 22:25:26 +0800

    refactor: extract  function enrichPerformance

--- src/statement.ts
+++ src/statement.ts
@@ -3,9 +3,13 @@ interface statementType {
   performances?: Record<string, any>
 }
 export function statement(invoice, plays) {
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    return result
+  }
   const statementData: statementType = {}
   statementData.customer = invoice.customer
-  statementData.performances = invoice.performances
+  statementData.performances = invoice.performances.map(enrichPerformance)
   return renderPlainText(statementData, plays)
 }
 
