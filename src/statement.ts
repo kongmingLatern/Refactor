c9536b5d029c71902f8f0a5d2228d2ad0856022b
凤之兮原 <1577310596@qq.com>
2022-08-16 21:57:26 +0800

    refactor: rename format params

--- src/statement.ts
+++ src/statement.ts
@@ -26,12 +26,12 @@ export function statement(invoice, plays) {
     return plays[aPerformance.playID]
   }
 
-  function volumeCreditsFor(perf: any) {
+  function volumeCreditsFor(aPerformance: any) {
     let result = 0
-    result += Math.max(perf.audience - 30, 0);
+    result += Math.max(aPerformance.audience - 30, 0);
 
-    if ("comedy" === playFor(perf).type) {
-      result += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(aPerformance).type) {
+      result += Math.floor(aPerformance.audience / 5);
     }
     return result
   }
