f881f9e69cd472849060580f6b0455ab3ce47132
凤之兮原 <1577310596@qq.com>
2022-08-18 22:42:10 +0800

    refactor: put amount and volumeCredits to class

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -1,14 +1,44 @@
-import { statementType } from "./type"
+import { performancesType, playType, statementType } from "./type"
 
 class PerformanceCalculator {
-  performance: any
-  play: any
+  performance: performancesType
+  play: playType
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance
     this.play = aPlay
   }
-}
 
+  get amount() {
+    let result = 0
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30)
+        }
+        break;
+      case "comedy":
+        result = 30000
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20)
+        }
+        result += 300 * this.performance.audience
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`)
+    }
+    return result
+  }
+
+  get volumeCredits() {
+    let result = 0
+    result += Math.max(this.performance.audience - 30, 0)
+    if ("comedy" === this.play.type) {
+      result += Math.floor(this.performance.audience / 5)
+    }
+    return result
+  }
+}
 export function createStatementData(invoice, plays) {
   const result: statementType = {}
   result.customer = invoice.customer
@@ -25,46 +55,15 @@ export function createStatementData(invoice, plays) {
     return data.performances.reduce((total, perf) => total + perf.amount, 0)
   }
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance)
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
   // In order to remove temporary variable, so we can extract the variable to function 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
-  }
-  // calculate total amount for this invoice
-  function amountFor(aPerformance: any) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 300 * aPerformance.audience;
-        }
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-  // calculate amount of this volume
-  function volumeCreditsFor(aPerformance: any) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    if ("comedy" === (aPerformance.play.type)) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-    return result
   }
 }
