ca975ccc2051280ee0b40f9f25a4e06eebbd48a2
凤之兮原 <1577310596@qq.com>
2022-08-18 22:11:24 +0800

    refactor: put usd to global

--- src/renderPlainText.ts
+++ src/renderPlainText.ts
@@ -1,16 +1,7 @@
+import { usd } from "./statement";
 import { statementType } from "./type";
 
 export function renderPlainText(data: statementType) {
-
-  // The function assigned to a temporary variable  (mean: currency) 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber);
-  }
-
 
   let result = `Statement for ${data.customer}\n`;
 
