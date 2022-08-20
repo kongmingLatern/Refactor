5dc2cca4c64eda728b762adb757a41995e97d999
凤之兮原 <1577310596@qq.com>
2022-08-20 21:44:45 +0800

    chore: add example

---
+++ src/Learn/Variable/example/template.ts
@@ -0,0 +1,20 @@
+class Order {
+  private _data: Record<string, any>
+  constructor(aRecord) {
+    this._data = aRecord
+  }
+
+  get quantity() {
+    return this._data.quantity
+  }
+
+  get itemPrice() {
+    return this._data.itemPrice
+  }
+
+  get price() {
+    return this.quantity * this.itemPrice -
+      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+      Math.min(this.quantity * this.itemPrice * 0.1, 100)
+  }
+}
