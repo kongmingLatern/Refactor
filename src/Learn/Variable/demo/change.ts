b22ce856289bbc668b014939f12ca9b8f2619c57
凤之兮原 <1577310596@qq.com>
2022-08-20 21:30:50 +0800

    chore: add a demo

---
+++ src/Learn/Variable/demo/change.ts
@@ -0,0 +1,7 @@
+function testVariable_change() {
+  const order: any = {}
+  const basePrice = order.quantity * order.itemPrice
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100)
+  return basePrice - quantityDiscount + shipping
+}
