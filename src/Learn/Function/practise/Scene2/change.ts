d9356271b8b806658f616788f4d74876be4c911f
凤之兮原 <1577310596@qq.com>
2022-08-20 21:09:11 +0800

    extract: extract function to global

--- src/Learn/Function/practise/Scene2/change.ts
+++ src/Learn/Function/practise/Scene2/change.ts
@@ -1,3 +1,8 @@
+/**
+ * 有局部变量的场景
+ * 
+ * @param invoice Object
+ */
 function printOwing_change1(invoice) {
   let outstanding = 0
 
@@ -7,11 +12,15 @@ function printOwing_change1(invoice) {
     outstanding += o.amount
   }
 
-  const today = new Date()
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+  recordDueDate(invoice)
 
   // print detail
   printDetails(invoice, outstanding)
+}
+
+function recordDueDate(invoice: any) {
+  const today = new Date()
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
 }
 
 function printDetails(invoice: any, outstanding: number) {
