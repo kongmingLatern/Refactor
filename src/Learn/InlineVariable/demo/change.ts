9c7a9e1b054fd1a5004f84a056ea7cccef400b58
凤之兮原 <1577310596@qq.com>
2022-08-21 19:19:26 +0800

    chore: add inlineVariable

---
+++ src/Learn/InlineVariable/demo/change.ts
@@ -0,0 +1,3 @@
+function inlineVariable_change(anOrder) {
+  return anOrder.basePrice > 1000
+}
---
+++ src/Learn/InlineVariable/demo/template.ts
@@ -0,0 +1,4 @@
+function inlineVariable(anOrder) {
+  let basePrice = anOrder.basePrice
+  return basePrice > 1000
+}
