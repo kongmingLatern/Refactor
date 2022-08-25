56141a0637dfe4021d2e20dc978dc883fb7a7aa2
凤之兮原 <1577310596@qq.com>
2022-08-25 14:38:41 +0800

    refactor: refacot Move Function practise

--- src/Learn/Move Function/practise/change.ts
+++ src/Learn/Move Function/practise/change.ts
@@ -1,37 +1,34 @@
 function trackSummary_change(points: any) {
   const totalTime = calculateTime()
-  const totalDistance = calculateDistance(points)
-  const pace = totalTime! / 60 / totalDistance!
+  const pace = totalTime! / 60 / calculateDistance(points)
   return {
     time: totalTime,
-    distance: totalDistance,
+    distance: calculateDistance(points),
     pace: pace
   }
-  function calculateTime() {
-    return undefined;
-  }
 }
-
-
+function calculateTime() {
+  return undefined;
+}
 function calculateDistance(points) {
   let result = 0
   for (let i = 0; i < points.length; i++) {
     result += distance(points[i - 1], points[i])
   }
   return result
-  function distance(p1, p2) {
-    const EARTH_RADIUS = 3959
-    const dLat = radians(p2.lat) - radians(p1.lat)
-    const dLon = radians(p2.lon) - radians(p1.lon)
-    const a = Math.pow(Math.sin(dLat / 2), 2)
-      + Math.cos(radians(p2.lat))
-      * Math.cos(radians(p1.lat))
-      * Math.pow(Math.sin(dLon / 2), 2)
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-    return EARTH_RADIUS * c
-  }
-  function radians(param1) {
-    // Implement
-    return 1;
-  }
+}
+function distance(p1, p2) {
+  const EARTH_RADIUS = 3959
+  const dLat = radians(p2.lat) - radians(p1.lat)
+  const dLon = radians(p2.lon) - radians(p1.lon)
+  const a = Math.pow(Math.sin(dLat / 2), 2)
+    + Math.cos(radians(p2.lat))
+    * Math.cos(radians(p1.lat))
+    * Math.pow(Math.sin(dLon / 2), 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  return EARTH_RADIUS * c
+}
+function radians(param1) {
+  // Implement
+  return 1;
 }
