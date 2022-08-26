f4295c5753801c6014a4f36ae51c635864803368
凤之兮原 <1577310596@qq.com>
2022-08-26 20:09:09 +0800

    chore: add demo for combineFunctiontToclass

---
+++ src/Learn/Combine Function Into Class/demo/change.ts
@@ -0,0 +1,11 @@
+class Reading {
+  base() {
+
+  }
+  taxableCharge() {
+
+  }
+  calculateBaseCharge() {
+
+  }
+}
---
+++ src/Learn/Combine Function Into Class/demo/template.ts
@@ -0,0 +1,9 @@
+function base(aReading: any) {
+
+}
+function taxableCharge(aReading: any) {
+
+}
+function calculateBaseCharge(aReading: any) {
+
+}
